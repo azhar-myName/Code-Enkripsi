2e2199e1b981dd38e3ac6492459e5d3abc67b2b9
azhar-myName <azharnumber21@gmail.com>
2024-08-27 15:05:50 +0700

    Update Enkripsi xor.cpp

--- Enkripsi xor.cpp
+++ Enkripsi xor.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 //enkripsi text dengan xor
-string enkripsiTeks(string plain_Text,char keys){
+string enkripsiTeks(string plain_Text,string keys){
 	string hasil = plain_Text;
+	int loop_keys = keys.size();
 	for(int i = 0; i < plain_Text.length(); i++){
-		hasil[i] = plain_Text[i] ^ keys;
+		hasil[i] = plain_Text[i] ^ keys[i % loop_keys];
 	}
 	return hasil;
 }
 //dekripsi text
-string dekripsiTeks(string hasil_enkripsi,char keys){
+string dekripsiTeks(string hasil_enkripsi,string keys){
 	string plain_Text = hasil_enkripsi;
+	int loop_keys = keys.size();
 	for(int i = 0; i < hasil_enkripsi.length(); i++){
-		plain_Text[i] = hasil_enkripsi[i] ^ keys;
+		plain_Text[i] = hasil_enkripsi[i] ^ keys[i % loop_keys];
 	}
 	return plain_Text;
 }
@@ -23,12 +25,12 @@ string dekripsiTeks(string hasil_enkripsi,char keys){
 int main(){
 	//variabel untuk enkripsi xor
 	string plain_Text;
-	char keys;
+	string keys;
 	//input
 	cout << "Masukan Teks: ";
 	getline(cin, plain_Text);
 	cout << "Masukan Keys: ";
-	cin >> keys;
+	getline(cin, keys);
 	//variabel output
 	string hasil_enkripsi = enkripsiTeks(plain_Text,keys);
 	cout << "Hasil Enkripsi: " << hasil_enkripsi << endl;
